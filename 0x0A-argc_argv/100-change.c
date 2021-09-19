d931c8fc06ed5bb5cde4fc2d8d06e43179133c9a
Eyena Samuel Baker <samuelbaker.ey@gmail.com>
2021-09-19 15:12:41 -0400

    update

--- 0x0A-argc_argv/100-change.c
+++ 0x0A-argc_argv/100-change.c
@@ -27,3 +27,29 @@ int returnCents(int n)
 	}
 	return (coins);
 }
+
+/**
+ * main - prints the minimum number of coins to
+ * make change for an amount of money
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0 if no errors
+ */
+int main(int argc, char **argv)
+{
+	int number;
+
+	if (argc != 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	number = atoi(argv[1]);
+	if (number < 0)
+	{
+		printf("0\n");
+		return (0);
+	}
+	printf("%i\n", returnCents(number));
+	return (0);
+}
