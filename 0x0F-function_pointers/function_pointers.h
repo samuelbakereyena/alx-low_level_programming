af24c38f6f663fd5fa00c01402e076e305640b21
Eyena Samuel Baker <samuelbaker.ey@gmail.com>
2021-09-29 17:40:18 -0400

    update

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -3,9 +3,10 @@
 
 #include <stddef.h>
 
-int _putchar(char c)
+int _putchar(char);
 void print_name(char *name, void (*f)(char *));
-void array_iteration(int *array, size_t size, void (*action)(int));
+void array_iterator(int *array, size_t size, void (*action)(int));
 int int_index(int *array, int size, int (*cmp)(int));
-int (*get_op_func(char *s))(int, int);
+
 #endif /* _FUNCTION_POINTERS_H_ */
+
